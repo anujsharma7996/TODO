b46901797817c648692e11d3b5be758ca1ca9a25
anujsharma7996 <sharma.anuj7996@gmail.com>
2021-08-08 01:46:27 +0530

    add a controller and a router

---
+++ controllers/home.controller.js
@@ -0,0 +1,3 @@
+module.exports.home = function (req, res) {
+    return res.end('<h1> Expresss is up </h1>')
+}
--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// using express router
+app.use('/', require('./routes'));
+
+
+// listening to the port
 app.listen(port, (err) => {
     if (err) {
         console.log(`Error in running the server: ${err}`);
